27e07216504086a55b8125cee9288a49777b2b4c
Mark Brown <mjbrown@microsoft.com>
2020-08-21 16:39:40 -0700

    Rewrite lab 1, update pkgs, net 3.1

--- dotnet/solutions/01-CreatingMultiPartition/Program.cs
+++ dotnet/solutions/01-CreatingMultiPartition/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
 
@@ -13,85 +10,106 @@ public class Program
 
     public static async Task Main(string[] args)
     {
-        using (CosmosClient client = new CosmosClient(_endpointUri, _primaryKey))
+        using CosmosClient client = new CosmosClient(_endpointUri, _primaryKey);
+
+        Database database = await InitializeDatabase(client, "EntertainmentDatabase");
+
+        Container container = await InitializeContainer(database, "EntertainmentContainer");
+
+        await LoadFoodAndBeverage(container);
+
+        await LoadTelevision(container);
+
+        await LoadMapViews(container);
+        
+    }
+
+    private static async Task<Database> InitializeDatabase(CosmosClient client, string databaseId)
+    {
+        DatabaseResponse databaseResponse = await client.CreateDatabaseIfNotExistsAsync("EntertainmentDatabase");
+        Database targetDatabase = databaseResponse.Database;
+        await Console.Out.WriteLineAsync($"Database Id:\t{targetDatabase.Id}");
+        return targetDatabase;
+    }
+
+    private static async Task<Container> InitializeContainer(Database database, string containerId)
+    {
+        IndexingPolicy indexingPolicy = new IndexingPolicy
         {
-            DatabaseResponse databaseResponse = await client.CreateDatabaseIfNotExistsAsync("EntertainmentDatabase");
-            Database targetDatabase = databaseResponse.Database;
-            await Console.Out.WriteLineAsync($"Database Id:\t{targetDatabase.Id}");
-
-            ContainerResponse response = await targetDatabase.CreateContainerIfNotExistsAsync("DefaultCollection", "/id");
-            Container defaultContainer = response.Container;
-            await Console.Out.WriteLineAsync($"Default Container Id:\t{defaultContainer.Id}");
-
-            IndexingPolicy indexingPolicy = new IndexingPolicy
+            IndexingMode = IndexingMode.Consistent,
+            Automatic = true,
+            IncludedPaths =
             {
-                IndexingMode = IndexingMode.Consistent,
-                Automatic = true,
-                IncludedPaths =
+                new IncludedPath
                 {
-                    new IncludedPath
-                    {
-                        Path = "/*"
-                    }
+                    Path = "/*"
                 }
-            };
-            
-            ContainerProperties containerProperties = new ContainerProperties("CustomCollection", $"/{nameof(IInteraction.type)}")
+            },
+            ExcludedPaths =
             {
-                IndexingPolicy = indexingPolicy,
-            };
-            var containerResponse = await targetDatabase.CreateContainerIfNotExistsAsync(containerProperties, 10000);
-            var customContainer = containerResponse.Container;
-            //var customContainer = targetDatabase.GetContainer("CustomCollection");
-            await Console.Out.WriteLineAsync($"Custom Container Id:\t{customContainer.Id}");
-
-            var foodInteractions = new Bogus.Faker<PurchaseFoodOrBeverage>()
-                .RuleFor(i => i.id, (fake) => Guid.NewGuid().ToString())
-                .RuleFor(i => i.type, (fake) => nameof(PurchaseFoodOrBeverage))
-                .RuleFor(i => i.unitPrice, (fake) => Math.Round(fake.Random.Decimal(1.99m, 15.99m), 2))
-                .RuleFor(i => i.quantity, (fake) => fake.Random.Number(1, 5))
-                .RuleFor(i => i.totalPrice, (fake, user) => Math.Round(user.unitPrice * user.quantity, 2))
-                .GenerateLazy(500);
-
-            foreach (var interaction in foodInteractions)
-            {
-                ItemResponse<PurchaseFoodOrBeverage> result = await customContainer.CreateItemAsync(interaction);
-                await Console.Out.WriteLineAsync($"Item Created\t{result.Resource.id}");
-            }
-
-            var tvInteractions = new Bogus.Faker<WatchLiveTelevisionChannel>()
-                .RuleFor(i => i.id, (fake) => Guid.NewGuid().ToString())
-                .RuleFor(i => i.type, (fake) => nameof(WatchLiveTelevisionChannel))
-                .RuleFor(i => i.minutesViewed, (fake) => fake.Random.Number(1, 45))
-                .RuleFor(i => i.channelName, (fake) => fake.PickRandom(new List<string> { "NEWS-6", "DRAMA-15", "ACTION-12", "DOCUMENTARY-4", "SPORTS-8" }))
-                .GenerateLazy(500);
-
-            foreach (var interaction in tvInteractions)
-            {
-                ItemResponse<WatchLiveTelevisionChannel> result = await customContainer.CreateItemAsync(interaction);
-                await Console.Out.WriteLineAsync($"Item Created\t{result.Resource.id}");
-            }
-
-            var mapInteractions = new Bogus.Faker<ViewMap>()
-                .RuleFor(i => i.id, (fake) => Guid.NewGuid().ToString())
-                .RuleFor(i => i.type, (fake) => nameof(ViewMap))
-                .RuleFor(i => i.minutesViewed, (fake) => fake.Random.Number(1, 45))
-                .GenerateLazy(500);
-
-            foreach (var interaction in mapInteractions)
-            {
-                ItemResponse<ViewMap> result = await customContainer.CreateItemAsync(interaction);
-                await Console.Out.WriteLineAsync($"Item Created\t{result.Resource.id}");
-            }
-
-            FeedIterator<GeneralInteraction> query = customContainer.GetItemQueryIterator<GeneralInteraction>("SELECT * FROM c");
-            while (query.HasMoreResults)
-            {
-                foreach (GeneralInteraction interaction in await query.ReadNextAsync())
+                new ExcludedPath
                 {
-                    Console.Out.WriteLine($"[{interaction.type}]\t{interaction.id}");
+                    Path = "/\"_etag\"/?"
                 }
             }
+        };
+
+        ContainerProperties containerProperties = new ContainerProperties(containerId, "/type")
+        {
+            IndexingPolicy = indexingPolicy,
+        };
+        
+        ContainerResponse containerResponse = await database.CreateContainerIfNotExistsAsync(containerProperties, 10000);
+        Container container = containerResponse.Container;
+        await Console.Out.WriteLineAsync($"Container Id:\t{container.Id}");
+        return container;
+    }
+
+    private static async Task LoadFoodAndBeverage(Container container)
+    {
+        var foodInteractions = new Bogus.Faker<PurchaseFoodOrBeverage>()
+            .RuleFor(i => i.id, (fake) => Guid.NewGuid().ToString())
+            .RuleFor(i => i.type, (fake) => nameof(PurchaseFoodOrBeverage))
+            .RuleFor(i => i.unitPrice, (fake) => Math.Round(fake.Random.Decimal(1.99m, 15.99m), 2))
+            .RuleFor(i => i.quantity, (fake) => fake.Random.Number(1, 5))
+            .RuleFor(i => i.totalPrice, (fake, user) => Math.Round(user.unitPrice * user.quantity, 2))
+            .GenerateLazy(500);
+
+        foreach (var interaction in foodInteractions)
+        {
+            ItemResponse<PurchaseFoodOrBeverage> result = await container.CreateItemAsync(interaction, new PartitionKey(interaction.type));
+            await Console.Out.WriteLineAsync($"Item Created\t{result.Resource.id}");
+        }
+    }
+
+    private static async Task LoadTelevision(Container container)
+    {
+        var tvInteractions = new Bogus.Faker<WatchLiveTelevisionChannel>()
+            .RuleFor(i => i.id, (fake) => Guid.NewGuid().ToString())
+            .RuleFor(i => i.type, (fake) => nameof(WatchLiveTelevisionChannel))
+            .RuleFor(i => i.minutesViewed, (fake) => fake.Random.Number(1, 45))
+            .RuleFor(i => i.channelName, (fake) => fake.PickRandom(new List<string> { "NEWS-6", "DRAMA-15", "ACTION-12", "DOCUMENTARY-4", "SPORTS-8" }))
+            .GenerateLazy(500);
+
+        foreach (var interaction in tvInteractions)
+        {
+            ItemResponse<WatchLiveTelevisionChannel> result = await container.CreateItemAsync(interaction, new PartitionKey(interaction.type));
+            await Console.Out.WriteLineAsync($"Item Created\t{result.Resource.id}");
+        }
+    }
+
+    private static async Task LoadMapViews(Container container)
+    {
+        var mapInteractions = new Bogus.Faker<ViewMap>()
+            .RuleFor(i => i.id, (fake) => Guid.NewGuid().ToString())
+            .RuleFor(i => i.type, (fake) => nameof(ViewMap))
+            .RuleFor(i => i.minutesViewed, (fake) => fake.Random.Number(1, 45))
+            .GenerateLazy(500);
+
+        foreach (var interaction in mapInteractions)
+        {
+            ItemResponse<ViewMap> result = await container.CreateItemAsync(interaction);
+            await Console.Out.WriteLineAsync($"Item Created\t{result.Resource.id}");
         }
     }
 }
